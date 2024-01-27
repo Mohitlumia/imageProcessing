b7c20d329dc562b20ed86a75127cfc0ddb7ea222
Mohit Gupta <mohitproduct@gmail.com>
2024-01-27 07:02:01 +0530

    entropy applied

--- scikit_image/4.entropy.py
+++ scikit_image/4.entropy.py
@@ -3,6 +3,15 @@ from matplotlib import pyplot as plt
 
 img = io.imread("sample_images/4scratch.jpg")
 
-plt.imshow(img, cmap = "gray")
 
 # scratch can not be seperated using threshold based segmentation
+# here entropy comes into play it uses texture to make image thresholdable
+
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+
+entropy_img = entropy(img, disk(3))
+
+plt.imshow(entropy_img,cmap = "gray")
+
+# now the entropy_img can be segmented using threshold
