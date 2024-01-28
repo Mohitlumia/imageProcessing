a7cac8ad53ee201f2d150b3ab3642cd72b44aa8e
Mohit Gupta <mohitproduct@gmail.com>
2024-01-28 21:26:33 +0530

    semgemted

--- scikit_image/6.histogram_based_segmentation.py
+++ scikit_image/6.histogram_based_segmentation.py
@@ -13,4 +13,23 @@ sigma_est = np.mean(estimate_sigma(img, channel_axis=-1))
 nlm_img = denoise_nl_means(img,h = 1.15*sigma_est, fast_mode=True, patch_size=5, patch_distance=3)
 nlm_ubyte_img = img_as_ubyte(nlm_img)
 
-plt.hist(nlm_ubyte_img.flat,bins=100, range=(0,255))
+# picking segments based on peeks shown in the histogram
+# creating binary image of each segment
+segm1 = (nlm_ubyte_img <= 27)
+segm2 = (27 < nlm_ubyte_img) & (nlm_ubyte_img <= 55)
+segm3 = (55 < nlm_ubyte_img) & (nlm_ubyte_img <= 95)
+segm4 = (95 < nlm_ubyte_img) & (nlm_ubyte_img <= 125)
+segm5 = (125 < nlm_ubyte_img) & (nlm_ubyte_img <= 200)
+segm6 = (200 < nlm_ubyte_img)
+
+# assign differnt color to each segment
+all_segm = np.zeros((nlm_ubyte_img.shape[0], nlm_ubyte_img.shape[1], 3))
+
+all_segm[segm1] = (1,0,0)
+all_segm[segm2] = (0,1,0)
+all_segm[segm3] = (0,0,1)
+all_segm[segm4] = (1,1,0)
+all_segm[segm5] = (0,1,1)
+all_segm[segm6] = (1,0,1)
+
+plt.imshow(all_segm)
