2fcc5fff1a1605a7d151eec79bc809b7afc80be4
Mohit Gupta <mohitproduct@gmail.com>
2024-01-28 20:32:44 +0530

    histogram_based_segmentation initial commit

---
+++ scikit_image/6.histogram_based_segmentation.py
@@ -0,0 +1,16 @@
+
+from skimage import io, img_as_float, img_as_ubyte
+from skimage.restoration import estimate_sigma, denoise_nl_means
+from matplotlib import pyplot as plt
+import numpy as np
+
+img = img_as_float(io.imread("sample_images/6BSE_Google_noisy.jpg"))
+
+# denoising using nlm_denoising_filter
+
+sigma_est = np.mean(estimate_sigma(img, channel_axis=-1))
+
+nlm_img = denoise_nl_means(img,h = 1.15*sigma_est, fast_mode=True, patch_size=5, patch_distance=3)
+nlm_ubyte_img = img_as_ubyte(nlm_img)
+
+plt.imsave("processed_images/6denoise_img.jpg", nlm_ubyte_img)
