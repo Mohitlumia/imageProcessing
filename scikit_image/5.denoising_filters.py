5f5b4b24f1f37acb30b2c90286a8d8ac5543c539
Mohit Gupta <mohitproduct@gmail.com>
2024-01-28 09:09:24 +0530

    gaussian denoising applied and save

--- scikit_image/5.denoising_filters.py
+++ scikit_image/5.denoising_filters.py
@@ -6,4 +6,12 @@ img = io.imread("sample_images/5noisy_img.jpg")
 # we may not be able to compare original and processed image
 # so lets save them in a separate folder name processed_images
 
-plt.imsave("processed_images/5original.jpg",img)
+# now lets apply gaussian filter to denoise our original image
+
+from scipy import ndimage as nd
+
+gaussian_img = nd.gaussian_filter(img, sigma = 3)
+# sigma represents standard deviation from mean
+# sigma 1 = 68%, sigma 2 = 95% and sigma 3 = 99.7%
+
+plt.imsave("processed_images/5gaussian.jpg", gaussian_img)
