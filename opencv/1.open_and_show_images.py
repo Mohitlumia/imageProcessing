e65e989826d4faa215159b145512ced8c6d42337
Mohit Gupta <mohitproduct@gmail.com>
2024-01-31 07:12:36 +0530

    cv2.split(img)

--- opencv/1.open_and_show_images.py
+++ opencv/1.open_and_show_images.py
@@ -4,9 +4,7 @@ import cv2
 img = cv2.imread("sample_images/1RGBY.jpg",1)
 
 # cv2.imread read image as BGR blue green red
-blue = img[:,:,0]
-green = img[:,:,1]
-red = img[:,:,2]
+blue, green, red = cv2.split(img)
 
 cv2.imshow("blue channel", blue)
 cv2.imshow("green channel", green)
