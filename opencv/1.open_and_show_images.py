162797376a0e648a3ea83ec5cb4ae67f2f701c5d
Mohit Gupta <mohitproduct@gmail.com>
2024-01-31 07:10:55 +0530

    spliting channels

--- opencv/1.open_and_show_images.py
+++ opencv/1.open_and_show_images.py
@@ -1,8 +1,14 @@
 #%%
 import cv2
 
-img = cv2.imread("sample_images/1einstein.jpg",0)
-# second arg in cv.imread 0,1,2 represents gray, colour and alpha channel
-# print(img.shape)
-cv2.imshow("einstein original image", img)
+img = cv2.imread("sample_images/1RGBY.jpg",1)
+
+# cv2.imread read image as BGR blue green red
+blue = img[:,:,0]
+green = img[:,:,1]
+red = img[:,:,2]
+
+cv2.imshow("blue channel", blue)
+cv2.imshow("green channel", green)
+cv2.imshow("red channel", red)
 cv2.waitKey(0)
