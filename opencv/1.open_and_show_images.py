6bb7ccf76eee7228b21dbc3a87c7a813e3f52d34
Mohit Gupta <mohitproduct@gmail.com>
2024-01-31 07:00:54 +0530

    cv2.imread shape

--- opencv/1.open_and_show_images.py
+++ opencv/1.open_and_show_images.py
@@ -1,7 +1,8 @@
 #%%
 import cv2
 
-img = cv2.imread("sample_images/1einstein.jpg")
-
+img = cv2.imread("sample_images/1einstein.jpg",0)
+# second arg in cv.imread 0,1,2 represents gray, colour and alpha channel
+# print(img.shape)
 cv2.imshow("einstein original image", img)
 cv2.waitKey(0)
