d0609f9befacf121a24b57c8f8734eff2839dc9b
Mohit Gupta <mohitproduct@gmail.com>
2024-02-03 07:59:19 +0530

    clhe applied

--- opencv/3CLAHE_and_thresholding.py
+++ opencv/3CLAHE_and_thresholding.py
@@ -1,4 +1,4 @@
-# CLHE contrast limiting histogram equalization
+# CLAHE contrast limiting histogram equalization
 import cv2
 import numpy as np
 
@@ -7,7 +7,13 @@ img = cv2.imread("opencv/sample_images/3Alloy.jpg",0)
 # equalize img or stratches histogram
 eq_img = cv2.equalizeHist(img)
 
+# eq_img is very noisy as it's equlized globly
+# for equalizing localy we use CALHE
+clhe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl_img = clhe.apply(img)
+
 cv2.imshow("Original image", img)
 cv2.imshow("Equalized image", eq_img)
+cv2.imshow("CLHE image", cl_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
