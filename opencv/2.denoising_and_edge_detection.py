f92f230056c538f7ef3a571993df7ca84ce99809
Mohit Gupta <mohitproduct@gmail.com>
2024-02-03 06:43:25 +0530

    blur

--- opencv/2.denoising_and_edge_detection.py
+++ opencv/2.denoising_and_edge_detection.py
@@ -6,7 +6,9 @@ img = cv2.imread("sample_images/2BSE_Google_noisy.jpg", 1)
 # creating my own bluring filter
 kernel = np.ones((3,3),np.float32)/9
 filter_2D = cv2.filter2D(img,-1,kernel)
+blur = cv2.blur(img,(3,3))
 
+cv2.imshow("Blur filter", blur)
 cv2.imshow("Custom filter", filter_2D)
 cv2.imshow("Original", img)
 cv2.waitKey(0)
