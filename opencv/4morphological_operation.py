9b8cfb5b85d0dff9b1364d68334ae368665729dc
Mohit Gupta <mohitproduct@gmail.com>
2024-02-12 22:50:02 +0530

    and closing = dilation + erosion

--- opencv/4morphological_operation.py
+++ opencv/4morphological_operation.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("sample_images/4BSE_Google_noisy.jpg",0)
 
-plt.hist(img.flat,bins=256,range=(0,255))
+#plt.hist(img.flat,bins=256,range=(0,255))
 
 # applying Otsu based thresholding
 ret,outs = cv2.threshold(img,0,255,cv2.THRESH_OTSU+cv2.THRESH_BINARY)
@@ -22,7 +22,11 @@ dilation = cv2.dilate(erosion,kernel,iterations=1)
 # opening in cv2 is equal to erosion + dilation
 opening = cv2.morphologyEx(outs,cv2.MORPH_OPEN,kernel)
 
-cv2.imshow("Erosion+Dilation image",dilation)
+# opening = erosion + dilation
+# closing = dilation + erosion
+closing = cv2.morphologyEx(outs,cv2.MORPH_CLOSE,kernel)
+
 cv2.imshow("Opening",opening)
+cv2.imshow("Closing",closing)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
