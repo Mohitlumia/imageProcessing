66bfea4b2a22b5bfac406875e0ee9ddef842c36c
Mohit Gupta <mohitproduct@gmail.com>
2024-02-23 21:18:50 +0530

    Good features to track

--- opencv/5.key-points_detectors_and_descriptors.py
+++ opencv/5.key-points_detectors_and_descriptors.py
@@ -14,5 +14,25 @@ harris = cv2.cornerHarris(gray,2,3,0.04)
 # keeping only one percent of harris.max()
 img[harris>0.01*harris.max()] = [225,0,0]
 
-cv2.imshow("Harris", img)
+#cv2.imshow("Harris", img)
+#cv2.waitKey(0)
+
+####################################################
+
+# Good Features to Track
+
+img = cv2.imread("opencv/sample_images/5grains.jpg")
+
+# it detectes corners of features in the image
+corners = cv2.goodFeaturesToTrack(gray,maxCorners=50,qualityLevel=0.01,minDistance=10)
+
+# it output the coordinate of corner points
+corners = np.int0(corners)
+
+# iterating throght each point and puting circle
+for coordinate in corners:
+    x,y = coordinate.ravel()
+    cv2.circle(img,(x,y),3,255,-1)
+
+cv2.imshow("Corner",img)
 cv2.waitKey(0)
