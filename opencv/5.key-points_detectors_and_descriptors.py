d4ca5ee4cb2db9411e2dfa455af1f70c35a163b6
Mohit Gupta <mohitproduct@gmail.com>
2024-02-26 07:06:12 +0530

    draw rich key point

--- opencv/5.key-points_detectors_and_descriptors.py
+++ opencv/5.key-points_detectors_and_descriptors.py
@@ -64,7 +64,7 @@ orb = cv2.ORB_create(50)
 
 kp, des = orb.detectAndCompute(img,None)
 
-img2 = cv2.drawKeypoints(img,kp,None,flags=0)
+img2 = cv2.drawKeypoints(img,kp,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.imshow("ORB", img2)
 cv2.waitKey(0)
