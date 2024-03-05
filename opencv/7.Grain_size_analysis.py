66016763879489f16e224dc46c2b2f49631aea19
Mohit Gupta <mohitproduct@gmail.com>
2024-03-06 00:00:06 +0530

    save csv

--- opencv/7.Grain_size_analysis.py
+++ opencv/7.Grain_size_analysis.py
@@ -48,6 +48,32 @@ clusters = measure.regionprops(label_image=label_mask, intensity_image=img)
 # lets extract those properties
 # clusters[0] means first grain
 
-for grain in clusters:
-    print(f"label: {grain.label} has {grain.area * pixel_size_um**2} um^2 area")
+propList = ['Area',
+            'equivalent_diameter', #Added... verify if it works
+            'orientation', #Added, verify if it works. Angle btwn x-axis and major axis.
+            'MajorAxisLength',
+            'MinorAxisLength',
+            'Perimeter',
+            'MinIntensity',
+            'MeanIntensity',
+            'MaxIntensity']
 
+output_file = open('opencv/sample_image/image_measurements.csv', 'w')
+output_file.write(',' + ",".join(propList) + '\n') #join strings in array by commas, leave first cell blank
+#First cell blank to leave room for header (column names)
+
+for grain in clusters:
+    #output cluster properties to the excel file
+    output_file.write(str(grain['Label']))
+    for i,prop in enumerate(propList):
+        if(prop == 'Area'): 
+            to_print = grain[prop]*pixels_to_um**2   #Convert pixel square to um square
+        elif(prop == 'orientation'): 
+            to_print = grain[prop]*57.2958  #Convert to degrees from radians
+        elif(prop.find('Intensity') < 0):          # Any prop without Intensity in its name
+            to_print = grain[prop]*pixels_to_um
+        else: 
+            to_print = grain[prop]     #Reamining props, basically the ones with Intensity in its name
+        output_file.write(',' + str(to_print))
+    output_file.write('\n')
+output_file.close()   #Closes the file, otherwise it would be read only.
