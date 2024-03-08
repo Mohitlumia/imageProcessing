be58ab8ccaf13a2c4e4da9a18ec6487a80217c84
Mohit Gupta <mohitproduct@gmail.com>
2024-03-08 15:46:50 +0530

    rotate

--- pillow/1.geometric_operations.py
+++ pillow/1.geometric_operations.py
@@ -1,5 +1,7 @@
 
 from PIL import Image
+import numpy as np
+import cv2
 
 image = Image.open("pillow/sample_images/1lenna.png")
 
@@ -11,3 +13,11 @@ new_hight = height
 resize_image = image.resize((new_width, new_hight))
 
 resize_image.save("pillow/processed_images/1resize_lenna.png")
+
+## Rotate
+
+theta = 45
+rotate_image = image.rotate(theta)
+
+rotate_image.save("pillow/processed_images/1rotate_lenna.png")
+
