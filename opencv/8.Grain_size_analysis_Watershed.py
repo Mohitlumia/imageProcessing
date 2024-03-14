2cb691c8c94e88206119fb6da7e51e8c8dc5bfb8
Mohit Gupta <mohitproduct@gmail.com>
2024-03-14 07:14:54 +0530

    add csv file

--- opencv/8.Grain_size_analysis_Watershed.py
+++ opencv/8.Grain_size_analysis_Watershed.py
@@ -50,4 +50,48 @@ cv2.imshow("watershed", img_color)
 cv2.imshow("colored grains",img_grain_color)
 cv2.waitKey(0)
 
-cv2.imwrite("opencv/processed_image/8watershed.jpg", img_color)
+cv2.imwrite("opencv/processed_image/8Grain_Analysis/8watershed.jpg", img_color)
+
+# now lets get the properties of our labeled grain
+from skimage import measure
+
+clusters = measure.regionprops(label_image=markers, intensity_image=img)
+# intensity_image is optional
+
+# clusters contains regional properties
+# lets extract those properties
+# clusters[0] means first grain
+
+propList = ['Area',
+            'equivalent_diameter', #Added... verify if it works
+            'orientation', #Added, verify if it works. Angle btwn x-axis and major axis.
+            'MajorAxisLength',
+            'MinorAxisLength',
+            'Perimeter',
+            'MinIntensity',
+            'MeanIntensity',
+            'MaxIntensity']
+
+output_file = open("opencv/processed_image/8Grain_Analysis/8image_measurements.csv", "w")
+output_file.write(',' + ",".join(propList) + '\n') #join strings in array by commas, leave first cell blank
+#First cell blank to leave room for header (column names)
+
+for grain in clusters:
+    #output cluster properties to the excel file
+    output_file.write(str(grain['Label']))
+    for i,prop in enumerate(propList):
+        if(prop == 'Area'):
+            #Convert pixel square to um square
+            to_print = grain[prop]*pixel_size_um**2
+        elif(prop == 'orientation'):
+            #Convert to degrees from radians
+            to_print = grain[prop]*57.2958
+        elif(prop.find('Intensity') < 0):
+            # intensity properties
+            to_print = grain[prop]*pixel_size_um
+        else:
+            #Reamining props
+            to_print = grain[prop]
+        output_file.write(',' + str(to_print))
+    output_file.write('\n')
+output_file.close()
